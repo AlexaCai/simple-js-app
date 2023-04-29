678932591028319ad38aba35d7f8148302d41c05
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-04-28 20:17:06 -0600

    This commit has added a variable with an array containing different objects

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    { name: 'balbuzard', height: 7, types: ['grass', 'poison'] },
+    { name: 'charmander', height: 6, types: ['fire', 'air'] },
+    { name: 'squirtle', height: 5, types: ['water', 'grass'] }
+];
