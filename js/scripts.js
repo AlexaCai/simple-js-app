d91cc4ea6475ce63ba0f5237910e471582cf4def
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-04-30 23:30:54 -0600

    This commit has created a loop with conditionals to display Pokemon names

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ let pokemonList = [
     { name: 'charmander', height: 6, types: ['fire', 'air'] },
     { name: 'squirtle', height: 5, types: ['water', 'grass'] }
 ];
+
+// This piece of code write down on the webpage all pokemon names from the array, as well as their respective heights and an highlight message for those very tall
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 6) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, thats big! ');
+    }
+    else {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+    }
+}
