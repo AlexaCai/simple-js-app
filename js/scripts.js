0a1ffe94ac6787f6c91a28534036b04cee80a6ea
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-05-06 17:08:53 -0600

    This commit has wrapped up the initial pokemonList inside an IIFE and used a forEach loop to iterates over each Pokemon in the repository
    The forEach loop used a function returned by the IIFE in order to retrieve the pokemonList array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,49 @@
-let pokemonList = [
-    { name: 'balbuzard', height: 7, types: ['grass', 'poison'] },
-    { name: 'charmander', height: 6, types: ['fire', 'air'] },
-    { name: 'squirtle', height: 5, types: ['water', 'grass'] }
-];
+//Sarting PokemonList - global data
+//let pokemonList = [
+//{ name: 'balbuzard', height: 7, types: ['grass', 'poison'] },
+//{ name: 'charmander', height: 6, types: ['fire', 'air'] },
+//{ name: 'squirtle', height: 5, types: ['water', 'grass'] }
+//];
 
-// This piece of code write down on the webpage all pokemon names from the array, as well as their respective heights and an highlight message for those very tall
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 6) {
-        document.write("<br>" + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, thats big! ');
+// OLD *forEach* method using the global pokemonList upper
+//pokemonList.forEach(function (pokemon) {
+//console.log("<br>" + pokemon.name + ': height of ' + pokemon.height + 'm / ' + pokemonList.types);
+//document.write("<br>" + pokemon.name + ': height of ' + pokemon.height + 'm / ' + pokemonList.types);
+//});
+
+//IIFE making pokemonList local to the function (rather than global if outside of the IIFE, as shown upper)
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        { name: 'balbuzard', height: 7, types: ['grass', ' poison'] },
+        { name: 'charmander', height: 6, types: ['fire', ' air'] },
+        { name: 'squirtle', height: 5, types: ['water', ' grass'] }
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        getAll: getAll,
+        add: add({ name: 'pikachu', height: 4, types: 'electric' })
+    };
+})();
+
+
+//NEW *forEach* method using an inside local function within the IIFE upper to retrieve pokemonList array
+//since its not possible anymore to reach the pokemonList outside of that IIFE.
+pokemonRepository.getAll().forEach(function (pokemon) {
+    if (pokemon.height > 6) {
+        console.log("<br>" + pokemon.name + ': height (' + pokemon.height + 'm - thats very big!) / type(s) (' + pokemon.types + ')');
+        document.write("<br>" + pokemon.name + ': height (' + pokemon.height + 'm - thats very big!) / type(s) (' + pokemon.types + ')');
     }
     else {
-        document.write("<br>" + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+        console.log("<br>" + pokemon.name + ': height (' + pokemon.height + 'm) / type(s) (' + pokemon.types + ')');
+        document.write("<br>" + pokemon.name + ': height (' + pokemon.height + 'm) / type(s) (' + pokemon.types + ')');
     }
-}
+})
