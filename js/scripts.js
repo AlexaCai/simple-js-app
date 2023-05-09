87b95014b40e3412820cfc21c9bf1ba70b9e1440
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-05-08 23:54:47 -0600

    This commit has reorganized the code and put out some code into comment to keep them while not make them interfere with new codes

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
-//Sarting PokemonList - global data
+//***Exercise 1.2 - Initial PokemonList - global data
 //let pokemonList = [
 //{ name: 'balbuzard', height: 7, types: ['grass', 'poison'] },
 //{ name: 'charmander', height: 6, types: ['fire', 'air'] },
 //{ name: 'squirtle', height: 5, types: ['water', 'grass'] }
 //];
 
-// OLD *forEach* method using the global pokemonList upper
+//***Exercise 1.3 - This piece of code write down on the webpage all pokemon names from the array, as well as their respective heights and an highlight message for those very tall
+//for (let i = 0; i < pokemonList.length; i++) {
+//if (pokemonList[i].height > 6) {
+//document.write("<br>" + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, thats big! ');
+//}
+//else {
+//document.write("<br>" + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+//}
+//}
+
+//***Exercise 1.5 - forEach method using the global pokemonList from Exercise 1.2 and replacing the FOR loop used in Exercise 1.3 to iterate over your repository array
 //pokemonList.forEach(function (pokemon) {
 //console.log("<br>" + pokemon.name + ': height of ' + pokemon.height + 'm / ' + pokemonList.types);
 //document.write("<br>" + pokemon.name + ': height of ' + pokemon.height + 'm / ' + pokemonList.types);
 //});
 
-//IIFE making pokemonList local to the function (rather than global if outside of the IIFE, as shown upper)
+//***Exercise 1.5 - IIFE making pokemonList from Exercise 1.2 local to the function (rather than global if outside of the IIFE, as shown upper)
 let pokemonRepository = (function () {
 
     let pokemonList = [
@@ -28,22 +38,50 @@ let pokemonRepository = (function () {
         return pokemonList;
     }
 
+    //*** Exercise 1.6 - 
+    function addListItem(pokemon) {
+        //let pokemonList = document.querySelector('.pokemon-list'); select the class of the ul element in the HTML file
+        let pokemonList = document.querySelector('.pokemon-list');
+        //let listItem = document.createElement('li'); created a li (list)
+        let listItem = document.createElement('li');
+        //let button = document.createElement('button'); created a buttons elements (or buttons tags) for each Pokemon
+        let button = document.createElement('button');
+        //button.innerText = pokemon.name; inserted the name of a pokemon inside each of the buttons created
+        //In this command, pokemon is equal to the (pokemon) parameter following the function in the code upper (function addListItem(pokemon) {)
+        //In this command, .name is equal to the name object inside the IIFE upper (as in let pokemonList = [{ name: 'balbuzard', height: 7, types: ['grass', ' poison'] },
+        button.innerText = pokemon.name;
+        //button.classList.add('pokemon-button-class'); added a class the the buttons created upper to be able to style them in CSS within styles.css
+        button.classList.add('pokemon-button-class');
+        //listItem.appendChild(button); attached each buttons to each li (list items) created previously for each Pokemon
+        listItem.appendChild(button);
+        //pokemonList.append(listItem); attached each li (list items) to the ul (unordered list - the parent element)
+        pokemonList.append(listItem);
+    }
+
     return {
         getAll: getAll,
-        add: add({ name: 'pikachu', height: 4, types: 'electric' })
+        add: add({ name: 'pikachu', height: 4, types: 'electric' }),
+        addListItem: addListItem
     };
 })();
 
 
-//NEW *forEach* method using an inside local function within the IIFE upper to retrieve pokemonList array
-//since its not possible anymore to reach the pokemonList outside of that IIFE.
+//***Exercise 1.5 - forEach method using an inside local function within the IIFE upper to retrieve pokemonList array since its not possible anymore to reach the pokemonList outside of that IIFE
+//pokemonRepository.getAll().forEach(function (pokemon) {
+//if (pokemon.height > 6) {
+//console.log("<br>" + pokemon.name + ': height (' + pokemon.height + 'm - thats very big!) / type(s) (' + pokemon.types + ')');
+//document.write("<br>" + pokemon.name + ': height (' + pokemon.height + 'm - thats very big!) / type(s) (' + pokemon.types + ')');
+//}
+//else {
+//console.log("<br>" + pokemon.name + ': height (' + pokemon.height + 'm) / type(s) (' + pokemon.types + ')');
+//document.write("<br>" + pokemon.name + ': height (' + pokemon.height + 'm) / type(s) (' + pokemon.types + ')');
+//}
+//})
+
+//***Exercise 1.6 
+//This function pokemonRepository.getAll().forEach(function (pokemon) { will run the loop over the function below (pokemonRepository.addListItem(pokemon);)
 pokemonRepository.getAll().forEach(function (pokemon) {
-    if (pokemon.height > 6) {
-        console.log("<br>" + pokemon.name + ': height (' + pokemon.height + 'm - thats very big!) / type(s) (' + pokemon.types + ')');
-        document.write("<br>" + pokemon.name + ': height (' + pokemon.height + 'm - thats very big!) / type(s) (' + pokemon.types + ')');
-    }
-    else {
-        console.log("<br>" + pokemon.name + ': height (' + pokemon.height + 'm) / type(s) (' + pokemon.types + ')');
-        document.write("<br>" + pokemon.name + ': height (' + pokemon.height + 'm) / type(s) (' + pokemon.types + ')');
-    }
+    //***This function pokemonRepository.addListItem(pokemon); which refere/is same to the addListItem function inside the IIFE upper will run as long as there is a new Pokemon in the IIFE pokemonList array to go over on
+    pokemonRepository.addListItem(pokemon);
+
 })
