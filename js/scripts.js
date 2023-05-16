c34685a6c94988a38f20ce0ee1386f7d1a2c1173
AlexaCai <131326250+AlexaCai@users.noreply.github.com>
2023-05-15 23:08:45 -0600

    This commit has added different functions and commands to fetch and display data from an external Pokemon API

--- js/scripts.js
+++ js/scripts.js
@@ -24,14 +24,19 @@
 //***Exercise 1.5 - IIFE making pokemonList from Exercise 1.2 local to the function (rather than global if outside of the IIFE, as shown upper)
 let pokemonRepository = (function () {
 
-    let pokemonList = [
-        { name: 'balbuzard', height: 7, types: ['grass', ' poison'] },
-        { name: 'charmander', height: 6, types: ['fire', ' air'] },
-        { name: 'squirtle', height: 5, types: ['water', ' grass'] }
-    ];
+    //***Exercise 1.7 - Load the list of pokemons on my website from an external source via the API
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add(pokemon) {
-        pokemonList.push(pokemon);
+        if (
+            typeof pokemon === "object" &&
+            "name" in pokemon
+        ) {
+            pokemonList.push(pokemon);
+        } else {
+            console.log("pokemon is not correct");
+        }
     }
 
     function getAll() {
@@ -62,13 +67,55 @@ let pokemonRepository = (function () {
         pokemonList.append(listItem);
     };
 
+    //***Function loadList used to get the complete list of Pokemon from here: https://pokeapi.co/api/v2/pokemon/?limit=150, precised in the 'let' apiURL at the beginning/top of this IIFE. 
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+                console.log(pokemon); //***This console.log(pokemon); add all the pokemons to the console, so when opening the console of the website, all pokemons appear.
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
+    //***Exercise 1.7 - We want each item to have a name and a detailsUrl property - so we use the detailsUrl property to load the detailed data for a given Pokémon. For this, we add a loadDetails() function, which takes a Pokémon item as an argument.
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            //***Below is adding the details to the item
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+
+    //Exercise 1.7 - Using showDetails() function to execute loadDetails() function in the IIFE upper. The showDetails() function is executed when a user clicks on a Pokémon. That’s the moment when Pokémon’s details are taken from the server acces to with the API. 
+    function showDetails(pokemon) {
+        loadDetails(pokemon).then(function () {
+            console.log(pokemon);
+        });
+    }
+
     return {
         getAll: getAll,
-        add: add({ name: 'pikachu', height: 4, types: 'electric' }),
-        addListItem: addListItem
+        add: add, //({ name: 'pikachu', height: 4, types: 'electric' }) the object on the left following the add:add is to manually add a Pokemon with caracteristics (ex here: Pikachu with his eight and type)
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
+        showDetails: showDetails
     };
 })();
-
 
 //***Exercise 1.5 - forEach method using an inside local function within the IIFE upper to retrieve pokemonList array since its not possible anymore to reach the pokemonList outside of that IIFE
 //pokemonRepository.getAll().forEach(function (pokemon) {
@@ -82,14 +129,21 @@ let pokemonRepository = (function () {
 //}
 //})
 
+//***Exercise 1.7 - The LoadList() method fetches data from the API, then add each Pokémon in the fetched data to pokemonList with the add function implemented earlier
+pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
+});
+
 //***Exercise 1.6 
-//This function pokemonRepository.getAll().forEach(function (pokemon) { will run the loop over the function below (pokemonRepository.addListItem(pokemon);)
+//This function pokemonRepository.getAll().forEach(function (pokemon) will run the loop over the function below (pokemonRepository.addListItem(pokemon);)
 pokemonRepository.getAll().forEach(function (pokemon) {
     //***This function pokemonRepository.addListItem(pokemon); which refere/is same to the addListItem function inside the IIFE upper will run as long as there is a new Pokemon in the IIFE pokemonList array to go over on
     pokemonRepository.addListItem(pokemon);
 })
 
 //***Exercise 1.6 -  This function is there only to be called inside the addListItem function upper (inside the IIFE), and more specifically for the on click addEventListner. By calling this function here in the IIFE upper, the addEventListner is logging each pokemon object (all info) to the console everytime a pokemon button is clicked on.
-function showDetails(pokemon) {
-    console.log(pokemon);
-}
+//function showDetails(pokemon) {
+//console.log(pokemon);
+//}
